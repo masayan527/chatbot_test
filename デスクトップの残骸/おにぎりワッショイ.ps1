0d150fa03c5b1c6f1717020c10ea2ebd4f1c844d
masayan527 <127716001+masayan527@users.noreply.github.com>
2025-05-29 11:05:21 +0900

    Delete デスクトップの残骸 directory

--- デスクトップの残骸/おにぎりワッショイ.ps1
+++
@@ -1,9 +0,0 @@
-$Signature = @'
-  [DllImport("user32.dll")]
-  public static extern void mouse_event(long dwFlags, long dx, long dy, long cButtons, long dwExtraInfo);
-'@
-$MouseEvent = Add-Type -MemberDefinition $Signature -Name "Win32MouseEvent" -Namespace Win32Functions -PassThru
-while ($true) {                             #
-  Start-Sleep -s 30
-  $MouseEvent::mouse_event(1, 0, 0, 0, 0)
-}
